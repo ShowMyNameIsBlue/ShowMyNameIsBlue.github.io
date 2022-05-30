8293fa6388a61d7a8a6e4664eaab231fac153d0e
ShowMyNameIsBlue <41276833+ShowMyNameIsBlue@users.noreply.github.com>
2022-05-30 08:02:46 +0000

    Deploying to gh-pages from @ ShowMyNameIsBlue/ShowMyNameIsBlue.github.io@b72fd98c09625ccbf6cf4d6d7857de6563e421be 🚀

---
+++ assets/amazing.html.59fd1532.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,c as a}from"./app.0328fe13.js";const i={},r={id:"inlineFrameExample",title:"Inline Frame Example",width:"100%",height:"500",src:"https://paveldogreat.github.io/WebGL-Fluid-Simulation/"};function n(o,m){return t(),a("iframe",r,`
+`)}var _=e(i,[["render",n],["__file","amazing.html.vue"]]);export{_ as default};
